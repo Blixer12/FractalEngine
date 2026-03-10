c4bb777f067cefb69c13fda35688221692285ff1
Blixer12 <avila_liam@icloud.com>
2026-03-10 16:45:47 -0700

    Fixed void &operator to Fractal"___.h" &operator

---
+++ Pipeline.h
@@ -0,0 +1,64 @@
+#pragma once
+
+// Standard Library
+#include <string>
+#include <vector>
+
+#include "Device.h"
+
+namespace FractalEngine
+{
+    struct PipelineConfigInfo
+    {
+        PipelineConfigInfo() = default;
+        PipelineConfigInfo(const PipelineConfigInfo &) = delete;
+        PipelineConfigInfo &operator=(const PipelineConfigInfo &) = delete;
+
+        VkViewport Viewport;
+        VkRect2D Scissor;
+        VkPipelineInputAssemblyStateCreateInfo InputAssemblyInfo;
+        VkPipelineRasterizationStateCreateInfo RasterizationInfo;
+        VkPipelineMultisampleStateCreateInfo MultisampleInfo;
+        VkPipelineColorBlendAttachmentState ColorBlendAttachment;
+        VkPipelineColorBlendStateCreateInfo ColorBlendInfo;
+        VkPipelineDepthStencilStateCreateInfo DepthStencilInfo;
+        VkPipelineViewportStateCreateInfo ViewportInfo;
+        VkPipelineLayout PipelineLayout = nullptr;
+        VkRenderPass RenderPass = nullptr;
+        uint32_t Subpass = 0;
+    };
+
+    class FractalPipeline
+    {
+    public:
+        FractalPipeline(
+            FractalDevice &fractalDevice,
+            const PipelineConfigInfo &ConfigInfo,
+            const std::string &VertFilePath,
+            const std::string &FragFilePath);
+
+        ~FractalPipeline();
+
+        FractalPipeline(const std::string &FilePath) = delete;
+        FractalPipeline &operator=(const std::string &FilePath) = delete;
+
+        void Bind(VkCommandBuffer CommandBuffer);
+
+        static void DefaultPipelineConfigInfo(PipelineConfigInfo &ConfigInfo, uint32_t Width, uint32_t Height);
+
+    private:
+        static std::vector<char> ReadFile(const std::string &FilePath);
+
+        void CreateGraphicsPipeline(
+            const PipelineConfigInfo &ConfigInfo,
+            const std::string &VertFilePath,
+            const std::string &FragFilePath);
+
+        void CreateShaderModule(const std::vector<char> &code, VkShaderModule *ShaderModule);
+
+        FractalDevice &fractalDevice;
+        VkPipeline GraphicsPipeline;
+        VkShaderModule VertShaderModule;
+        VkShaderModule FragShaderModule;
+    };
+}
---
+++ Swapchain.h
@@ -0,0 +1,83 @@
+#pragma once
+
+#include "Device.h"
+
+// vulkan headers
+#include <vulkan/vulkan.h>
+
+// std lib headers
+#include <string>
+#include <vector>
+
+namespace FractalEngine
+{
+
+  class FractalSwapChain
+  {
+  public:
+    static constexpr int MAX_FRAMES_IN_FLIGHT = 2;
+
+    FractalSwapChain(FractalDevice &DeviceRef, VkExtent2D windowExtent);
+    ~FractalSwapChain();
+
+    FractalSwapChain(const FractalSwapChain &) = delete;
+    FractalSwapChain &operator=(const FractalSwapChain &) = delete;
+
+    VkFramebuffer getFrameBuffer(int index) { return SwapChainFrameBuffers[index]; }
+    VkRenderPass getRenderPass() { return renderPass; }
+    VkImageView getImageView(int index) { return SwapChainImageViews[index]; }
+    size_t imageCount() { return SwapChainImages.size(); }
+    VkFormat getSwapChainImageFormat() { return SwapChainImageFormat; }
+    VkExtent2D getSwapChainExtent() { return SwapChainExtent; }
+    uint32_t width() { return SwapChainExtent.width; }
+    uint32_t height() { return SwapChainExtent.height; }
+
+    float extentAspectRatio()
+    {
+      return static_cast<float>(SwapChainExtent.width) / static_cast<float>(SwapChainExtent.height);
+    }
+    VkFormat findDepthFormat();
+
+    VkResult acquireNextImage(uint32_t *ImageIndex);
+    VkResult submitCommandBuffers(const VkCommandBuffer *Buffers, uint32_t *ImageIndex);
+
+  private:
+    void createSwapChain();
+    void createImageViews();
+    void createDepthResources();
+    void createRenderPass();
+    void createFrameBuffers();
+    void createSyncObjects();
+
+    // Helper functions
+    VkSurfaceFormatKHR chooseSwapSurfaceFormat(
+        const std::vector<VkSurfaceFormatKHR> &AvailableFormats);
+    VkPresentModeKHR chooseSwapPresentMode(
+        const std::vector<VkPresentModeKHR> &AvailablePresentModes);
+    VkExtent2D chooseSwapExtent(const VkSurfaceCapabilitiesKHR &Capabilities);
+
+    VkFormat SwapChainImageFormat;
+    VkExtent2D SwapChainExtent;
+
+    std::vector<VkFramebuffer> SwapChainFrameBuffers;
+    VkRenderPass renderPass;
+
+    std::vector<VkImage> DepthImages;
+    std::vector<VkDeviceMemory> DepthImageMemorys;
+    std::vector<VkImageView> DepthImageViews;
+    std::vector<VkImage> SwapChainImages;
+    std::vector<VkImageView> SwapChainImageViews;
+
+    FractalDevice &Device;
+    VkExtent2D windowExtent;
+
+    VkSwapchainKHR SwapChain;
+
+    std::vector<VkSemaphore> ImageAvailableSemaphores;
+    std::vector<VkSemaphore> RenderFinishedSemaphores;
+    std::vector<VkFence> InFlightFences;
+    std::vector<VkFence> ImagesInFlight;
+    size_t CurrentFrame = 0;
+  };
+
+} // namespace lve
